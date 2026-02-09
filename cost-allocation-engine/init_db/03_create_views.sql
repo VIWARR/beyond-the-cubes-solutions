665a880d2cd118da4faa707b7a3afaa72056fcb5
VIWARR <dm.pavlyuchenko@gmail.com>
2026-02-09 22:38:50 +0300

    refactor: remove unnecessary line breaks in SQL script

--- cost-allocation-engine/init_db/03_create_views.sql
+++ cost-allocation-engine/init_db/03_create_views.sql
@@ -1,18 +1,9 @@
+-- Представление для внешних расходов
 create or replace view v_external_costs as
-select 
-	transaction_date,
-	cost_center,
-	cost_object,
-	amount
-from cost_transactions
-where cost_type = 'External';
+select transaction_date, cost_center, amount
+from cost_transactions where cost_type = 'External';
 
+-- Представление для внутренних расчетов между объектами затрат
 create or replace view v_internal_costs as
-select
-	transaction_date,
-	cost_center,
-	cost_object,
-	amount,
-	allocation_basis
-from cost_transactions
-where cost_type = 'Internal';
+select transaction_date, cost_center as source_cc, cost_object as target_cc, amount
+from cost_transactions where cost_type = 'Internal';
