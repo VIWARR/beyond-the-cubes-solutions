e8a20ff936fe682ef4546df786e8f87a9db8f46b
VIWARR <dm.pavlyuchenko@gmail.com>
2026-02-10 20:49:17 +0300

    feat: change from testing to prod worker

--- cost-allocation-engine/app/main.py
+++ cost-allocation-engine/app/main.py
@@ -1,26 +1,24 @@
+import time
 import logging
-import sys
-from sqlalchemy import text
-from utils.logger import setup_logging
-from db.session import get_db_session
+from app.db.session import get_db_session
+from app.services.orchestrator import Orchestrator
+from app.utils.logger import setup_logging
 
 setup_logging()
 logger = logging.getLogger(__name__)
 
-def check_connection():
-    logger.info("Тест соединения")
-    try:
-        db = get_db_session()
-        result = db.execute(text("select 1"))
-        
-        if result.fetchone():
-            logger.info("Соединение установлено")
+def main():
+    logger.info("Воркер запущен")
+    while True:
+        try:
+            with get_db_session() as session:
+                orchestrator = Orchestrator(session)
+                orchestrator.process_pending_tasks()
+                session.commit()
+        except Exception as e:
+            logger.error(f"Ошибка в цикле: {e}")
 
-        db.close()
-
-    except Exception as e:
-        logger.error(f"Ошибка {e}")
-        sys.exit(1)
+        time.sleep(10)
 
 if __name__ == "__main__":
-    check_connection()
+    main()
