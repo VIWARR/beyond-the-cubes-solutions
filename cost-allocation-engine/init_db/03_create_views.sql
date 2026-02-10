780027845ae5f9047d1e5549b3f54f390a85c2a3
VIWARR <dm.pavlyuchenko@gmail.com>
2026-02-10 20:52:40 +0300

    feat: add field receiver_cost_center

--- cost-allocation-engine/init_db/03_create_views.sql
+++ cost-allocation-engine/init_db/03_create_views.sql
@@ -5,5 +5,5 @@ from cost_transactions where cost_type = 'External';
 
 -- Представление для внутренних расчетов между объектами затрат
 create or replace view v_internal_costs as
-select transaction_date, cost_center as source_cc, cost_object as target_cc, amount
+select transaction_date, cost_center as source_cc, receiver_cost_center as target_cc, amount
 from cost_transactions where cost_type = 'Internal';
