704a1827f3ae9536063500d60a530e3576ab44a3
VIWARR <dm.pavlyuchenko@gmail.com>
2026-02-10 20:51:50 +0300

    feat: change the name of the table calculations_tasks

--- cost-allocation-engine/init_db/01_create_tables.sql
+++ cost-allocation-engine/init_db/01_create_tables.sql
@@ -3,6 +3,7 @@ create table public.cost_transactions (
 	transaction_id bigserial primary key,
 	transaction_date date not null,
 	cost_center varchar(100) not null,
+	receiver_cost_center varchar(100) not null,
 	cost_object varchar(100) not null,
 	cost_type varchar(10) not null check (cost_type in ('Internal', 'External')),
 	cost_category varchar(50),
@@ -15,7 +16,7 @@ create table public.cost_transactions (
 create index idx_transactions_date on cost_transactions(transaction_date);
 
 -- Служебная таблица
-create table calculations_tasks (
+create table calculation_tasks (
 	id serial primary key,
 	target_month date not null unique,
 	status varchar(20) not null default 'pending',
